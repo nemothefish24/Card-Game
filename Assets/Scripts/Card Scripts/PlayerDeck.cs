bb890dd512d315917d49ad58b99c8c7110fa2419
nemothefish24 <andrewkwon8@gmail.com>
2023-01-02 12:55:39 -0500

    List changes

--- Assets/Scripts/Card Scripts/PlayerDeck.cs
+++ Assets/Scripts/Card Scripts/PlayerDeck.cs
@@ -6,8 +6,24 @@ namespace AK{
 
 [CreateAssetMenu(menuName = "Holders/Player Deck Holder")]
 public class PlayerDeck : ScriptableObject
-{
+{   
+    ActorHolder actor;
+
+    public List<string> chosenDeck = new List<string>();
+
+    public List<string> startingDeck = new List<string>();
     public List<Card> deckCards = new List<Card>();
+
+    void Start()
+    {   
+        for (int i = 0; i < chosenDeck.Count; i++)
+        {
+            startingDeck[i] = chosenDeck[i];
+        
+        }
+
+        
+    }
 
 
 }
