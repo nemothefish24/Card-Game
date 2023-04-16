29d63afe98108fc04c0107ab14ddd7d6f6f1bc98
nemothefish24 <andrewkwon8@gmail.com>
2023-04-16 13:59:46 -0400

    merge

--- Assets/Scripts/Battle/ActionManager.cs
+++ Assets/Scripts/Battle/ActionManager.cs
@@ -7,6 +7,11 @@ public class ActionManager : MonoBehaviour
 {   
     public List <CardInstance> playerQueuedCards = new List<CardInstance>();
 
-    
+    ActorHolder actor;
+
+    public void UpdatePlayerQueuedCards() 
+    {
+        playerQueuedCards = actor.cardsDown;
+    }
 }
 }
