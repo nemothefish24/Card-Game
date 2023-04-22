46ebe44e5d6562e6b71b61b97638875b5057d1f5
nemothefish24 <andrewkwon8@gmail.com>
2023-04-22 12:17:05 -0400

    Revert "merge"
    This reverts commit 29d63afe98108fc04c0107ab14ddd7d6f6f1bc98.

--- Assets/Scripts/Battle/ActionManager.cs
+++ Assets/Scripts/Battle/ActionManager.cs
@@ -7,11 +7,6 @@ public class ActionManager : MonoBehaviour
 {   
     public List <CardInstance> playerQueuedCards = new List<CardInstance>();
 
-    ActorHolder actor;
-
-    public void UpdatePlayerQueuedCards() 
-    {
-        playerQueuedCards = actor.cardsDown;
-    }
+    
 }
 }
