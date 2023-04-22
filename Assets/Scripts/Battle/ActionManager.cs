6559f769c2d196f23ade2d9ecbc6ee9e9c3837b7
nemothefish24 <andrewkwon8@gmail.com>
2023-04-22 13:46:55 -0400

    Fixed Player Card Queueing

--- Assets/Scripts/Battle/ActionManager.cs
+++ Assets/Scripts/Battle/ActionManager.cs
@@ -2,16 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace AK{
+namespace AK
+{
 public class ActionManager : MonoBehaviour
 {   
     public List <CardInstance> playerQueuedCards = new List<CardInstance>();
 
     ActorHolder actor;
 
-    public void UpdatePlayerQueuedCards() 
-    {
-        playerQueuedCards = actor.cardsDown;
-    }
+    
 }
 }
---
+++ Assets/Scripts/Battle/UpdatePlayerQueue.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AK
+{
+public class UpdatePlayerQueue : MonoBehaviour
+{
+    public List <CardInstance> playerQueuedCards = new List<CardInstance>();
+
+    public ActorHolder actor;
+
+    public void UpdatePlayerQueuedCards() 
+    {
+        playerQueuedCards = actor.cardsDown;
+    }
+}
+}
